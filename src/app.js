e7161250ba3e01692134f1d3810094ca691ce5e4
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2025-08-20 17:39:45 +0530

    asdf

--- src/app.js
+++ src/app.js
@@ -89,8 +89,8 @@ MongoClient.connect(process.env.MONGO_URI)
       }
     });
 
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
+    app.listen(PORT, "127.0.0.1", () => {
+      console.log("Server running on port 3000");
     });
   })
   .catch((err) => {
